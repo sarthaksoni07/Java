c9a05e2d9ca9b443221930a1ce672dffab1122ad
sarthaksoni07 <sarthaksoni07@gmail.com>
2025-10-17 20:00:00 +0530

    generic methods with iteration

--- genMet.java
+++ genMet.java
@@ -1,10 +1,23 @@
-public class genMet{
+
+public class genMet {
+
     // generic method or function overlaoding 
-    public static <T> void Print(T item1){
+    public static <T> void Print(T item1) {
         System.out.println("item is " + item1);
     }
-    public static void main(String[] args){
-       Print("sarthak");
-       Print(1);
+
+    public static <T> void printArr(T[] arr) {
+        for (T element : arr) {
+            System.out.println(element);
+        }
     }
+
+    public static void main(String[] args) {
+        Print("sarthak");
+        Print(1);
+        Integer[] arr = {1, 3, 4, 5, 5};// we used the wrapper class becasue generic method only work with objects and not primitive data types
+        
+        printArr(arr);
+    }
+
 }
